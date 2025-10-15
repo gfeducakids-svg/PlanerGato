1bac96b805fc5990c759cf0836f5cc58ef1c257c
ALD Clothing <ald.clothingg@gmail.com>
2025-10-15 02:04:06 +0000

    Deixe abaixo da seção Resultados Visíveis (_for element <PersonalizedTip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
     <div className="flex flex-col min-h-[100dvh]">
       <main className="flex-1">
         <HeroSection />
-        <PersonalizedTipsSection />
         <ProblemSection />
         <SolutionSection />
         <BenefitsSection />
+        <PersonalizedTipsSection />
         <AuthoritySection />
         <SocialProofSection />
         <OfferSection />
